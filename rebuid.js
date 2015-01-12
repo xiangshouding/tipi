ce170eccda6f0b5f6259179e4ef50ad692e52945
xiangshouding <fansekey@gmail.com>
2015-01-12 15:57:58 +0800

    add gitbook SUMMARY.md & rebuild.js

---
+++ rebuid.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+var path = require('path');
+var ROOT = __dirname;
+
+function readIndex() {
+    return fs.readFileSync(path.join(ROOT, 'book', 'index.markdown'), {encoding: 'utf-8'});
+}
+
+function genSummary(c) {
+    fs.writeFileSync(path.join(ROOT, 'SUMMARY.md'), c);
+}
+
+function createLink(c, link) {
+    var reg = new RegExp('\\[' + link + '\\]:\\s+\\?p=([^\\n]+)');
+    var ret = '';
+    c.replace(reg, function(all, $1) {
+        if ($1) {
+            ret = 'book/' + $1 + '.markdown';
+        }
+    });
+    return ret;
+}
+
+function main() {
+    var c = readIndex();
+    var indexs = {};
+    var rArr = [];
+    cArr = c.split('\n');
+    cArr.forEach(function (line) {
+        var rLine = line.replace(/(?:\+|\*|-)\s\[([\s\S]+?)\]\[([\s\S]+?)\]/g, function (all, text, link) {
+            if (all) {
+                return all.replace(link, createLink(c, link));
+            }
+            return all;
+        });
+        if (line.match(/^\s*\[/)) {
+            return;
+        }
+        rArr.push(rLine);
+    });
+    c = rArr.join('\n');
+    genSummary(c);
+}
+
+main();
